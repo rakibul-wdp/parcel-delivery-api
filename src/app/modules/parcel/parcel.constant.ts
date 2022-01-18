ce21734cc86f103fe35ad262ff235edb8b1d3067
rakibul-wdp <rakibul.wdp@gmail.com>
2022-01-19 04:05:00 +0600

    create parcel constant

---
+++ src/app/modules/parcel/parcel.constant.ts
@@ -0,0 +1,15 @@
+export const parcelSearchableFields = [
+  "trackingId",
+  "receiver.name",
+  "receiver.email",
+  "receiver.phone",
+  "type",
+];
+
+export const parcelFilterableFields = [
+  "status",
+  "isBlocked",
+  "startDate",
+  "endDate",
+  "search",
+];
