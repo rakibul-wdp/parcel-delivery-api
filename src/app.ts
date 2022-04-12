c30129ce9f39db8719e5f87221b5a7aa7932031f
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-13 04:05:00 +0600

    update app file

--- src/app.ts
+++ src/app.ts
@@ -1,82 +1,52 @@
-import express from "express";
+// src/app.ts
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import express, { Request, Response } from "express";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-
-import { connectDB } from "./config/database";
-import authRoutes from "./routes/auth";
-import userRoutes from "./routes/users";
-import parcelRoutes from "./routes/parcels";
-
-dotenv.config();
+import { envVars } from "./app/config/env";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet());
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL
-        : "http://localhost:3000",
+    origin: envVars.FRONTEND_URL,
     credentials: true,
   })
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
 });
 app.use(limiter);
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/parcels", parcelRoutes);
+app.use("/api/v1", router);
 
 // Health check
-app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Server is running!" });
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running!",
+    timestamp: new Date().toISOString(),
+  });
 });
 
 // 404 handler
-app.use("*", (req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+app.use(notFound);
 
-// Error handler
-app.use(
-  (
-    err: any,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Something went wrong!" });
-  }
-);
-
-// Start server
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
