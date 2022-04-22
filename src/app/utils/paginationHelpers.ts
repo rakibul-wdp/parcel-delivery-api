437b51eab1c47f546d87f48466a1658f7e2ce26e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-04-23 04:05:00 +0600

    create pagination helper utility

---
+++ src/app/utils/paginationHelpers.ts
@@ -0,0 +1,32 @@
+interface IPaginationOptions {
+  page?: number;
+  limit?: number;
+  sortBy?: string;
+  sortOrder?: "asc" | "desc";
+}
+
+interface IPaginationResult {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: "asc" | "desc";
+}
+
+export const paginationHelpers = {
+  calculatePagination: (options: IPaginationOptions): IPaginationResult => {
+    const page = Number(options.page) || 1;
+    const limit = Number(options.limit) || 10;
+    const skip = (page - 1) * limit;
+    const sortBy = options.sortBy || "createdAt";
+    const sortOrder = options.sortOrder || "desc";
+
+    return {
+      page,
+      limit,
+      skip,
+      sortBy,
+      sortOrder,
+    };
+  },
+};
