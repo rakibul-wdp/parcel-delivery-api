5f002f692a0080a81c2eae06ea62307b9134447f
rakibul-wdp <rakibul.wdp@gmail.com>
2025-09-03 12:00:17 +0600

    fix env config

--- dist/config/env.js
+++ dist/config/env.js
@@ -3,9 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.env = void 0;
 const zod_1 = require("zod");
 const envSchema = zod_1.z.object({
-    NODE_ENV: zod_1.z
-        .enum(["development", "production", "test"])
-        .default("development"),
+    NODE_ENV: zod_1.z.enum(["development", "production", "test"]).default("production"),
     PORT: zod_1.z.string().default("3000"),
     MONGODB_URI: zod_1.z.string(),
     JWT_SECRET: zod_1.z.string(),
