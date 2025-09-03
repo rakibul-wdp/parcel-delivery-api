163738bee69294caf80b2158d0cf3dec337a963d
rakibul-wdp <rakibul.wdp@gmail.com>
2025-09-03 11:49:17 +0600

    set production default of env schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   PORT: z.string().default("3000"),
   MONGODB_URI: z.string(),
   JWT_SECRET: z.string(),
