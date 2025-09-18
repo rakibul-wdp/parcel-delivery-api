da9e325196eeb7771360285ae5a5b925892e4c40
rakibul-wdp <rakibul.wdp@gmail.com>
2025-09-18 11:51:15 +0600

    create error types interface

---
+++ src/app/interfaces/error.types.ts
@@ -0,0 +1,10 @@
+export interface TErrorSources {
+  path: string | number;
+  message: string;
+}
+
+export interface TGenericErrorResponse {
+  statusCode: number;
+  message: string;
+  errorSources?: TErrorSources[];
+}
