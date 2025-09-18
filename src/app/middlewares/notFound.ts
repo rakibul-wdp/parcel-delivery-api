57275bf40e6a3a95625eb1ce8afdfaebc7c756ea
rakibul-wdp <rakibul.wdp@gmail.com>
2025-09-18 11:57:16 +0600

    create not found middleware

---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+
+const notFound = (req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API Not Found",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+};
+
+export default notFound;
